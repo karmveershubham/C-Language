da294be0e450cbe1355fa44ae41bbdded8ef8e5a
Shubham Karmveer <s.karmveer1201@gmail.com>
2023-11-24 00:59:12 +0530

    fourtyfour.c

--- Lab/fourtyfour.c
+++ Lab/fourtyfour.c
@@ -1,3 +1,5 @@
+//Wap to print,delete copy the contents of one file to another file.
+
 #include <stdio.h>
 #include <stdlib.h> // For exit()
 int main(){
@@ -13,20 +15,27 @@ int main(){
    }
    printf("Enter the filename to open for writing ");
    scanf("%s", filename);
+
    // Open another file for writing
    fptr2 = fopen(filename, "w");
    if (fptr2 == NULL){
       printf("Cannot open file %s ", filename);
       exit(0);
    }
-   // Read contents from file
+   printf( "printing contents from file: \n");
+
    c = fgetc(fptr1);
    while (c != EOF){
       fputc(c, fptr2);
+      printf ("%c", c); 
       c = fgetc(fptr1);
    }
-   printf("Contents copied to %s", filename);
+   printf("\n\nContents copied to %s", filename);
    fclose(fptr1);
    fclose(fptr2);
+
+   printf("\n\nContents of %s deleted", filename);
+   fclose(fopen(filename, "w"));
+   
    return 0;
 }
